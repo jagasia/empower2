691cd43ff10445e698dab4262cde3d2b0f6cf2e7
Jagdeeswaran <t-Jagdeeswaran@cognizant.com>
2023-09-05 15:16:36 +0530

    05-Sep 15:16

---
+++ Java/05-sep-01/src/com/empower/demo/Main5.java
@@ -0,0 +1,15 @@
+package com.empower.demo;
+
+import java.util.Date;
+
+public class Main5 {
+
+	public static void main(String[] args) {
+		Object []arr=new Object[10];
+		arr[0]="India";
+		arr[1]=20;
+		arr[2]=1.5f;
+		arr[3]=new Date();
+	}
+
+}
