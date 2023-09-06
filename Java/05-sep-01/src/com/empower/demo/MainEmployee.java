3d4155e37fc357979966b114b1c3b59d2151db4d
Jagdeeswaran <t-Jagdeeswaran@cognizant.com>
2023-09-06 09:12:25 +0530

    06-Sep 09:12

--- Java/05-sep-01/src/com/empower/demo/MainEmployee.java
+++ Java/05-sep-01/src/com/empower/demo/MainEmployee.java
@@ -7,7 +7,7 @@ public class MainEmployee {
 	public static void main(String[] args) {
 		Employee arr[]=new Employee[5];
 		arr[0]=new Employee(12, "Steven", "King", 17000.0);
-		arr[1]=new Employee(7, "Abdul", "Raheem", 24000.0);
+		arr[1]=new Employee(12, "Abdul", "Raheem", 24000.0);
 		arr[2]=new Employee(14, "Skanda", "Smaran", 55000.0);
 		arr[3]=new Employee(6, "Komal", "Gubbi", 55000.0);
 		arr[4]=new Employee(8, "Chenna Keshava", "Reddy", 55000.0);
---
+++ Java/06-sep-01/src/App1.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class App1 {
+
+	public static void main(String[] args) {
+//		ArrayList<Integer> marks=new ArrayList<>();
+		List<Integer> marks=new ArrayList<>();
+		
+		marks=new LinkedList<>();
+//		marks.remove(index)
+//		marks.get(index)
+		
+	}
+
+}
