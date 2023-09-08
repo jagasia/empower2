ba8c957ecab8caac6321c9a7c9d328dcf8107a54
Jagdeeswaran <t-Jagdeeswaran@cognizant.com>
2023-09-08 11:25:54 +0530

    08-Sep 11:25

--- Java/dummy1/src/dummy1/App.java
+++ Java/dummy1/src/dummy1/App.java
@@ -9,10 +9,10 @@ public class App {
 
 	public static void main(String[] args) throws SQLException {
 		
-		Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE", "hr", "hr");
-		ResultSet rs = con.createStatement().executeQuery("SELECT * FROM EMPLOYEES");
-		while(rs.next())
-			System.out.println(rs.getString(2)+"\t"+rs.getString(3)+"\t"+rs.getString("SALARY"));
+		Connection con = DriverManager.getConnection("jdbc:oracle:thin:@todos_tpurgent", "MyDbUsername", "$3cR3tP4s$w0rD");
+//		ResultSet rs = con.createStatement().executeQuery("SELECT * FROM EMPLOYEES");
+//		while(rs.next())
+//			System.out.println(rs.getString(2)+"\t"+rs.getString(3)+"\t"+rs.getString("SALARY"));
 		System.out.println("Hello world");
 	}
 
