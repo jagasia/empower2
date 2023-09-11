1d94c191cd55534cf88829aa6807e8f46922fd4f
Jagdeeswaran <t-Jagdeeswaran@cognizant.com>
2023-09-11 10:59:49 +0530

    11-Sep 10:59

---
+++ Java/11-sep-student-dao/src/StudentDao.java
@@ -0,0 +1,17 @@
+import java.sql.SQLException;
+import java.util.List;
+
+public interface StudentDao {
+	public void createTable() throws SQLException;
+
+	void create(Student student) throws SQLException;
+
+	List<Student> read() throws SQLException;
+
+	Student read(Integer id) throws SQLException;
+
+	void update(Student student) throws SQLException;
+
+	void delete(Integer id) throws SQLException;
+
+}
--- Java/11-sep-student-dao/src/StudentDaoImpl.java
+++ Java/11-sep-student-dao/src/StudentDaoImpl.java
@@ -1,9 +1,22 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
-public class StudentDaoImpl {
+public class StudentDaoImpl implements StudentDao {
+	public void createTable() throws SQLException
+	{
+		Connection con = getMyConnection();
+		Statement st = con.createStatement();
+		st.execute("CREATE TABLE STUDENT(ID INT PRIMARY KEY,NAME VARCHAR2(20),ADDRESS VARCHAR2(50),PHONE VARCHAR2(13));");
+		con.close();
+	}
+	
+	@Override
 	public void create(Student student) throws SQLException {
 		//add a new row to the STUDENT table
 		Connection con = getMyConnection();
@@ -17,25 +30,58 @@ public class StudentDaoImpl {
 		con.close();
 	}
 	
-	public void read() throws SQLException {
+	@Override
+	public List<Student> read() throws SQLException {
 		Connection con = getMyConnection();
+		Statement st = con.createStatement();
+		ResultSet rs = st.executeQuery("SELECT * FROM STUDENT");
+		List<Student> studentList=new ArrayList<>();
+		while(rs.next())
+		{
+			Student student=new Student(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4));
+			studentList.add(student);
+		}
+		con.close();
+		return studentList;
+	}
+	@Override
+	public Student read(Integer id) throws SQLException {
+		Connection con = getMyConnection();
+		PreparedStatement st = con.prepareStatement("SELECT * FROM STUDENT WHERE ID=?");
+		st.setInt(1, id);
+		ResultSet rs = st.executeQuery();
+		Student student=null;
+		if(rs.next())
+		{
+			student=new Student(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4));
+		}
+		con.close();
+		return student;
+	}
+	@Override
+	public void update(Student student) throws SQLException {
+		Connection con = getMyConnection();
+		PreparedStatement st = con.prepareStatement("UPDATE STUDENT SET name=?, address=?, phone=? WHERE id=?");
+		st.setString(1, student.getName());
+		st.setString(2, student.getAddress());
+		st.setString(3, student.getPhone());
+		st.setInt(4, student.getId());
+		int no=st.executeUpdate();
+		System.out.println(no+" row updated");
 		con.close();
 	}
-	public void read(Integer id) throws SQLException {
+	@Override
+	public void delete(Integer id) throws SQLException {
 		Connection con = getMyConnection();
-		con.close();
-	}
-	public void update() throws SQLException {
-		Connection con = getMyConnection();
-		con.close();
-	}
-	public void delete() throws SQLException {
-		Connection con = getMyConnection();
+		PreparedStatement st = con.prepareStatement("DELETE FROM STUDENT WHERE ID=?");
+		st.setInt(1, id);
+		int no=st.executeUpdate();
+		System.out.println(no+" row deleted");
 		con.close();
 	}
 
 	private Connection getMyConnection() throws SQLException {
-		Connection con = DriverManager.getConnection("jdbc:h2:file:./data/sampledata","sa","");
+		Connection con = DriverManager.getConnection("jdbc:h2:file:./data/sampledata.mv","sa","");
 		return con;
 	}
 }
