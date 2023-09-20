2dbc710cfd9fc72a68f79d994729888a0729a013
Jagdeeswaran <t-Jagdeeswaran@cognizant.com>
2023-09-20 08:37:55 +0530

    20-Sep 08:37

---
+++ advJava/first-boot/src/main/java/com/empower/ecom/FirstBootApplication.java
@@ -0,0 +1,14 @@
+package com.empower.ecom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstBootApplication.class, args);
+		System.out.println("Hello world");
+	}
+
+}
