d8295280ee8f14143d3fdf66ed28146ab0017713
Jagdeeswaran <t-Jagdeeswaran@cognizant.com>
2023-10-04 14:14:53 +0530

    04-Oct 14:14

--- CSS/three.js
+++ CSS/three.js
@@ -1,2 +1,16 @@
-var dt=new Date(2023,01);
-console.log(dt);
+var emp={id:0,name:''};
+emp.id=200;
+emp.name='Raja';
+console.log(emp)
+
+
+
+// var dt=new Date(2023,01);
+// console.log(dt);
+
+// var str='{"id":"100","firstName":"Steven","lastName":"King","salary":"24000"}';
+// var employee=JSON.parse(str);
+// console.log(employee);
+
+// var x=JSON.stringify(employee);
+// console.log(x);
