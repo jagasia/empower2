2f864121c1fc500e787484c8207dd746976d77e5
Jagdeeswaran <t-Jagdeeswaran@cognizant.com>
2023-10-05 11:15:46 +0530

    05-Oct 11:15

--- React/first-project/src/App.js
+++ React/first-project/src/App.js
@@ -1,34 +1,13 @@
 import './App.css';
 import Hello from './Components/Hello';
+import Welcome from './Components/Welcome';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <div class="container-fluid p-5 bg-primary text-white text-center">
-  <h1>My First Bootstrap Page</h1>
-  <p>Resize this responsive page to see the effect!</p> 
-</div>
-  
-<div class="container mt-5">
-  <div class="row">
-    <div class="col-sm-4">
-      <h3>Column 1</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
-      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
-    </div>
-    <div class="col-sm-4">
-      <h3>Column 2</h3>
-      <Hello />
-    </div>
-    <div class="col-sm-4">
-      <h3>Column 3</h3>        
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
-      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
-    </div>
-  </div>
-</div>
-
+        <Hello />
+        <Welcome />
       </header>
     </div>
   );
--- React/first-project/src/Components/Hello.js
+++ React/first-project/src/Components/Hello.js
@@ -1,6 +1,8 @@
 import React from "react"
-export function Hello()
+export function Hello(props)
 {
-    return <h1>Hello world</h1>
+    // return <h1>Hello {props.name} and your nick name is {props.nickname} </h1>
+    // return React.createElement("p",null,React.createElement("i",null,"Hello world"))
+    return <p><i>Hello world</i></p>
 }
 export default Hello
