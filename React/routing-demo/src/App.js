9cfa1bb82befdd81365f5a1eb1a4312836737d87
Jagdeeswaran <t-Jagdeeswaran@cognizant.com>
2023-10-06 08:48:15 +0530

    06-Oct 08:48

--- React/routing-demo/src/App.js
+++ React/routing-demo/src/App.js
@@ -1,4 +1,9 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import { About } from './Components/About';
+import { Contact } from './Components/Contact';
+import { Home } from './Components/Home';
+import { Menu } from './Components/Menu';
 
 function App() {
   return (
@@ -6,12 +11,19 @@ function App() {
       <div class="container-fluid p-5 bg-primary text-center text-white">
       <h1>Welcome to Routing Demo</h1>
       </div>
+
+    <Menu />
+      
       <div class="row">
         <div class="col-sm-4">
           column 1
         </div>
         <div class="col-sm-4">
-          
+        <Routes>
+            <Route path='/About' element={ <About />  }></Route>
+            <Route path='/home' element={ <Home /> }></Route>
+            <Route path='/contact' element={ <Contact />}></Route>
+          </Routes>
         </div>
         <div class="col-sm-4">
           column 3
